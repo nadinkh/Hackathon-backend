68648b2e95eb2b89a433ab09f27652b5a91fb664
Benyamin Lea <lea.benyamin@student-cs.fr>
2021-01-20 12:38:56 +0100

    Login working

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -12,17 +12,17 @@ router.post("/login", (req, res) => {
           return res.status(400).json({ msg: "invalid credentials" });
         jwt.sign(
           {
-            id: newUser._id,
-            firstName: newUser.firstName,
-            lastName: newUser.lastName,
-            email: newUser.email,
-            type: newUser.type,
-            appointments: newUser.appointments,
-            cell: newUser.cell,
-            bloodType: newUser.bloodType,
-            previousDonor: newUser.previousDonor,
-            healthInsurance: newUser.healthInsurance,
-            IDNumber: newUser.IDNumber,
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            type: user.type,
+            appointments: user.appointments,
+            cell: user.cell,
+            bloodType: user.bloodType,
+            previousDonor: user.previousDonor,
+            healthInsurance: user.healthInsurance,
+            IDNumber: user.IDNumber,
           },
           process.env.TOKEN,
           (err, token) => {
