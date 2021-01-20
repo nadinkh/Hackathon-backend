175bd579864043df31545d358cf3120f17c9b42d
Benyamin Lea <lea.benyamin@student-cs.fr>
2021-01-20 09:58:05 +0100

    adding basic routes

---
+++ routers/usersRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const users = require("../models/users");
+
+router.get("/", (req, res) => {
+  users.find().then((users) => res.json(users));
+});
+
+router.get("/:id", (req, res) => {
+  users.findOne({ _id: req.params.id }).then((user) => res.json(user));
+});
+
+module.exports = router;
